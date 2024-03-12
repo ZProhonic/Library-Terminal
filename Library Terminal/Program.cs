8cfb32ab61e1d70f65627d7e3d664a323cab742f
Zack Prohonic <zackprohonic20@gmail.com>
2024-03-11 21:24:11 -0400

    Library Terminal Updates

--- Library Terminal/Program.cs
+++ Library Terminal/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string userSearch = "";
+
+Console.WriteLine("Welcome to our Library! We have an extensive catalog of books for rent.");
+Console.WriteLine("There are a few options to select a book of your choice. Please select one of the following:");
+Console.WriteLine("1. Display list of books 2. Search for a book by author 3. Search for a book by title keyword");
+
+int userSelection = 0;
+bool continueLooping = true;
+
+while (continueLooping)
+{
+    try
+    {
+        string input = Console.ReadLine();
+        userSelection = int.Parse(input);
+        continueLooping = false;
+    }
+    catch (FormatException ex) 
+    {
+        Console.WriteLine("Please enter a valid option");
+    }
+
+}
+while (true)
+{
+    if (userSelection == 1)
+    {
+        Console.WriteLine(); //loop through List of books
+        break;
+    }
+    else if (userSelection == 2)
+    {
+        Console.WriteLine("Please enter the name of the author");
+        userSearch = Console.ReadLine().ToLower().Trim();
+        break;
+    }
+    else if (userSelection == 3)
+    {
+        Console.WriteLine("Please enter a book title keyword");
+        userSearch = Console.ReadLine().ToLower().Trim();
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Please pick a way to search for a book");
+    }
+}
+
+
+//use linq to search List by usersearch. List<Book> userPick = books.Where(x => x.Author.ToLower().Trim() == userSearch).ToList();
+//List<Book> userPick = books.Where(x => x.Title.ToLower().Trim() == userSearch).ToList();
+// loop through books to select desired outcome: foreach (Book book in userPick) { Console.WriteLine(book.Author) }
+//foreach (Book book in userPick) { Console.WriteLine(book.Title) }
+
+
+
+
--- Library Terminal/obj/Debug/net8.0/Library Terminal.AssemblyInfo.cs
+++ Library Terminal/obj/Debug/net8.0/Library Terminal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library Terminal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b282ce8e118e347af7e97d465db07f29851fb9a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d14fd8edda985c91a35a41ba70122081151ee49")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library Terminal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library Terminal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
